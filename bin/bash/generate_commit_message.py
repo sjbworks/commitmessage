247a7c15de6d9054c2ee8ae5b229a5b3785c976b
sjbworks <sacred.jizo.bosatz@gmail.com>
2023-06-08 00:11:37 +0900

    fix: updated rules to reflect new message conditions

--- bin/bash/generate_commit_message.py
+++ bin/bash/generate_commit_message.py
@@ -12,7 +12,7 @@ openai.api_key = api_key
 diff_output = sys.stdin.read()
 
 # GPT-3へのリクエストを作成
-prompt = f"Given the following git diff output, suggest an appropriate commit message in English:\n\n{diff_output}\n\nCommit message:"
+prompt = f"Given the following git diff output, suggest an appropriate commit message in English, with a lowercase prefix at the beginning:\n\n{diff_output}\n\nPrefix rules should be as follows\n\nfeat: A new feature\n\nfix: A bug fix\n\ndocs: Documentation only changes\n\nstyle: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)\n\nrefactor: A code change that neither fixes a bug nor adds a feature\n\nperf: A code change that improves performance\n\ntest: Adding missing or correcting existing tests\n\nchore: Changes to the build process or auxiliary tools and libraries such as documentation generation\n\nCommit message:"
 response = openai.Completion.create(
     engine="text-davinci-003",
     prompt=prompt,
